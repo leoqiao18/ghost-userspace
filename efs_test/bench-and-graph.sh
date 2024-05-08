2d02c28b41ed32cbbf8e82a914410128cb1467a9
Yiming Fang <yf2484@columbia.edu>
2024-05-08 07:49:56 -0400

    update script

--- efs_test/bench-and-graph.sh
+++ efs_test/bench-and-graph.sh
@@ -8,6 +8,5 @@ base_watts=8.0
 sudo pkill "shiv.out"
 
 sudo python3 efs_test/efs_bench.py ${fname}-efs.csv ${interval} ${iterations} ${base_watts}
-python3 efs_test/plot_graph.py ${fname}-efs ${interval}
 sudo python3 efs_test/cfs_bench.py ${fname}-cfs.csv ${interval} ${iterations} ${base_watts}
-python3 efs_test/plot_graph.py ${fname}-cfs ${interval} ${iterations}
+python3 efs_test/plot_graph.py ${fname} ${interval} ${iterations}
